db53740e6f6386f53feeb44d9476a4b3e839de7f
Niels Hüsken <monarch@hotmail.de>
2018-08-24 13:49:36 +0200

    Festbuch2018_V1

--- site/views/helloworld/view.html.php
+++ site/views/helloworld/view.html.php
@@ -30,11 +30,11 @@ class HelloWorldViewHelloWorld extends JViewLegacy
 		$this->msg = $this->get('Msg');
 		$document = JFactory::getDocument();
 		$myDir = 'components/com_helloworld/views/helloworld/tmpl/';
-		$url = JUri::base() . $myDir . 'styles.3de4e0396b715684784b.css';
+		$url = JUri::base() . $myDir . 'styles.fe19a3fdce3431736387.css';
 		$document->addStyleSheet($url);
-		$url1 = JUri::base() . $myDir . 'polyfills.9a5f6d04e0781d28c53e.js';
-		$url2 = JUri::base() . $myDir . 'runtime.cf99c7b9e5f7f9b19078.js';
-		$url3 = JUri::base() . $myDir . 'main.3cdd7a3e6cf6e20739fb.js';
+		$url1 = JUri::base() . $myDir . 'polyfills.2f4a59095805af02bd79.js';
+		$url2 = JUri::base() . $myDir . 'runtime.a66f828dca56eeb90e02.js';
+		$url3 = JUri::base() . $myDir . 'main.57f81321ce051ccd55ca.js';
 		$document->addScript($url1);
 		$document->addScript($url2);
 		$document->addScript($url3, array() ,array('defer' => true));
